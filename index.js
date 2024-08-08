6e2fd374ab6ede0493bba6bbf430c28e6685dc61
kolamat <matanmidupe004@gmail.com>
2024-08-08 19:52:39 +0100

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   index.html
    	modified:   index.js

---
+++ index.js
@@ -0,0 +1,4 @@
+const nini = document.createElement("h1");
+nini.textContent =
+  "Welcome to your best and second year on earth wishing you Long life and more prosperity in life and a Happy Birthday ";
+document.querySelector("body").appendChild(nini);
