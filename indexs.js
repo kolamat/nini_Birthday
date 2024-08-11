647a61f80695ae721641861828c85512b4fc02c6
kolamat <matanmidupe004@gmail.com>
2024-08-11 16:10:51 +0100

    On branch main Your branch is ahead of 'origin/main' by 2 commits.   (use "git push" to publish your local commits)
    Changes to be committed:
    	modified:   indexs.js

--- indexs.js
+++ indexs.js
@@ -7,14 +7,14 @@ function startCountdown(startNumber) {
   const intervalId = setInterval(() => {
     currentNumber -= 1;
     countdownElement.textContent = currentNumber;
-    countdownElement.style.fontSize = "500px";
+    countdownElement.style.fontSize = "100px";
 
     if (currentNumber <= 0) {
       clearInterval(intervalId);
       countdownElement.textContent = "Time's up!";
       countdownElement.style.color = "#ff6b6b"; // Change color when time's up
     }
-  }, 1000);
+  }, 500);
 }
 
 startCountdown(15);
